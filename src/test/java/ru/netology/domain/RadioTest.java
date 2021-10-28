5d9b1da0ee8b1ea5efe6799232b4c69afe255792
anay333 <anay_1991@bk.ru>
2021-10-28 21:33:05 +0500

    громкость 500

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -39,8 +39,8 @@ class RadioTest {
 
     @Test
     public void shouldSetCurrentVolume() {
-        radio.setCurrentVolume(5);
-        int expected = 5;
+        radio.setCurrentVolume(500);
+        int expected = 0;
         assertEquals(expected, radio.getCurrentVolume());
     }
 
